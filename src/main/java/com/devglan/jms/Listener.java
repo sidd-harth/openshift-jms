8b39222cc48d9d03b57796e9b699d08bfe918e8d
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-01-09 10:22:18 +0530

    v1

---
+++ src/main/java/com/devglan/jms/Listener.java
@@ -0,0 +1,31 @@
+package com.devglan.jms;
+
+import com.google.gson.Gson;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import java.util.Map;
+
+@Component
+public class Listener {
+
+	@JmsListener(destination = "inbound.queue")
+	@SendTo("outbound.queue")
+	public String receiveMessage(final Message jsonMessage) throws JMSException {
+		String messageData = null;
+		System.out.println("Received message " + jsonMessage);
+		String response = null;
+		if(jsonMessage instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage)jsonMessage;
+			messageData = textMessage.getText();
+            Map<String, String> map = new Gson().fromJson(messageData, Map.class);
+            response  = "Hello " + map.get("name");
+		}
+		return response;
+	}
+
+}
